23139ea16ade8ac11a8cf4483ba941b771b57c87
kartikeypatel1 <kp0543985@gmail.com>
2024-10-17 23:51:09 +0530

    this is updated now

--- list.py
+++ list.py
@@ -1,3 +1,7 @@
- # a list of three elements
-ages = [19, 26, 29]
-print(ages)
+languages = ['Python', 'Swift', 'C++']
+
+# Access the first element
+print(languages[0])   # Python
+
+# Access the third element
+print(languages[2])   # C++
