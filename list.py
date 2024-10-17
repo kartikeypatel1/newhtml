3206ecb8df3be2fc13f01033001f2ccc79c377f3
kartikeypatel1 <kp0543985@gmail.com>
2024-10-17 23:40:24 +0530

    this is modified

--- list.py
+++ list.py
@@ -1,8 +1,3 @@
-list1=[8,9,'true',"hello",9,8.9]
-print(list1)
-# [8, 9, 'true', 'hello', 9, 8.9]
-print(list1[2])
-# true
-list1[1]=5
-print(list1)
-# [8, 5, 'true', 'hello', 9, 8.9]
+ # a list of three elements
+ages = [19, 26, 29]
+print(ages)
